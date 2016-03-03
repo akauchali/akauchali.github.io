811f63b43adee2083f2237575c8f7dcd11b9d838
akauchali <abdullah@capitalsigma.biz>
2016-03-03 08:03:12 +0200

    Setup

---
+++ clientconnectdemo/funds/3775.js
@@ -0,0 +1,281 @@
+
+{
+
+	"meta":
+	{
+		"version":1.0,
+		"displayOptions":
+		{
+			"performance":
+			{
+				"visible": true,
+				  "tabs":
+				  {
+					"cumulative": true,
+					"periods": true,
+					"drawdowns": true,
+					"statistics": true
+					}
+			},
+			"exposure":
+			{
+				  "visible": true,
+				  "tabs":
+				  {
+					"assetClass": true,
+					"sectors": true,
+					"creditRatings": true,
+					"managers": true,
+					"maturityBands": true,
+					"institutions": false,
+					"geographical": false
+				   }
+			},
+			"income":
+			{
+				"visible": true
+			}
+		}
+
+	},
+
+	"factsheetDate": "2015-12-31T00:00:00.000Z",
+	"fundName": "Hollard Prime Equity Fund",
+	"fundClass": "C",
+	"launched": "2014/02/01",
+	"jseCode": "HPEFP",
+	"isinCode": "ZAE993884729",
+	"pricePerUnit": "114.53c",
+	"priceDate": "2015-12-31T00:00:00.000Z",
+	"priceAtDate": "114.32c on 31/10/2018",
+	"riskRating": 2,
+	"riskRatingText": "Moderately Conservative",
+	"totalFundSizeZAR": 88542437.00,
+	"investmentHorizon": "Long term - 7 year rolling periods or longer",
+	"reg28Compliance": true,
+	"officialBenchmark": "Median of sector",
+	"incomeDistribution": "Semi annually (Feb and Aug)",
+
+
+	"fundLargeText":
+	{
+		"objective":"To assist investors to reach a return after fees of CPI +6% p.a. over rolling 7 year periods, through a spectrum of investments in the equity, bond, money or property markets and participatory interests in related collective investment scheme."
+		,"investmentMandate":
+		{
+			"heading": "The fund's investment policy requires that:",
+			"lines": 
+			[
+				{"li": "At least 70% of assets to be invested in South African markets"},
+				{"li":"Up to 25% of assets may be invested outside of Africa"},
+				{"li":"An additional 5% of assets may be invested in Africa (ex SA)"},
+				{"li":"Up to 75% of assets may be exposed to equities (including international equities)"},
+				{"li":"Up to 25% of assets may be exposed to property (including international property)"},
+				{"li":"The fund shall comply with Reg. 28 of the Pension Funds Act"}
+			]
+
+		}		
+	}
+
+	,"feesExpenses": 
+	
+		[
+            { "titleName": "Total Expense Ratio/TER (incl VAT as at 30 Sep 2015)", "titleValue": "1.28%" },
+            { "titleName": "Initial Management Fee (excl VAT)", "titleValue": "0.00%" },
+            { "titleName": "Annual Management Fee (excl VAT)", "titleValue": "1.10%" },
+            { "titleName": "Performance Fees", "titleValue": "Not Applicable" }
+         ]
+		,"feeNote":"The fees provided above exclude any fees charged by administration platforms."
+	,
+
+
+	"performance":
+	{
+		"periods":
+		[	
+		    {"period":"1 month","Fund":-3.18,"Benchmark":-3.43},
+			{"period":"3 months","Fund":-6.73,"Benchmark":-8.42},
+			{"period":"6 months","Fund":-5.90,"Benchmark":-6.28},
+			{"period":"9 months","Fund":-9.61,"Benchmark":-10.12},
+			{"period":"Year to date","Fund":5.18,"Benchmark":-3.43},
+			{"period":"1 year","Fund":-2.44,"Benchmark":-3.80},
+			{"period":"Since inception annualised","Fund":8.17,"Benchmark":5.65},
+			{"period":"Since inception cumulative","Fund":17.02,"Benchmark":11.63}
+		]
+
+		,"cumulative":
+		[
+			{"month":"2013-10","Fund":100,"Benchmark":100},
+			{"month":"2013-11","Fund":99.39706,"Benchmark":98.59495},
+			{"month":"2013-12","Fund":102.7910124,"Benchmark":101.5259018},
+			{"month":"2014-01","Fund":99.02751476,"Benchmark":99.1410076},
+			{"month":"2014-02","Fund":101.8476995,"Benchmark":102.8018587},
+			{"month":"2014-03","Fund":104.6151562,"Benchmark":105.4834606},
+			{"month":"2014-04","Fund":106.7507595,"Benchmark":107.6869468},
+			{"month":"2014-05","Fund":109.9541683,"Benchmark":109.666319},
+			{"month":"2014-06","Fund":112.6579633,"Benchmark":111.5859841},
+			{"month":"2014-07","Fund":114.2155836,"Benchmark":113.1931012},
+			{"month":"2014-08","Fund":114.480084,"Benchmark":112.518776},
+			{"month":"2014-09","Fund":114.8261115,"Benchmark":110.0899513},
+			{"month":"2014-10","Fund":116.2593251,"Benchmark":111.040556},
+			{"month":"2014-11","Fund":116.8764064,"Benchmark":112.3507124},
+			{"month":"2014-12","Fund":99.5678643,"Benchmark":112.1790349},
+			{"month":"2015-01","Fund":120.1608473,"Benchmark":115.045787},
+			{"month":"2015-02","Fund":125.545339,"Benchmark":119.0562313},
+			{"month":"2015-03","Fund":124.7368019,"Benchmark":118.4170065},
+			{"month":"2015-04","Fund":130.0718947,"Benchmark":123.1279074},
+			{"month":"2015-05","Fund":124.3650423,"Benchmark":118.9376431},
+			{"month":"2015-06","Fund":123.4005044,"Benchmark":117.8813103},
+			{"month":"2015-07","Fund":125.3597218,"Benchmark":118.0867715},
+			{"month":"2015-08","Fund":120.1552375,"Benchmark":114.6934124},
+			{"month":"2015-09","Fund":119.7324593,"Benchmark":113.2177325},
+			{"month":"2015-10","Fund":126.8959445,"Benchmark":120.8429751},
+			{"month":"2015-11","Fund":124.9004043,"Benchmark":117.3872406},
+			{"month":"2015-12","Fund":122.5057513,"Benchmark":114.596177}
+		]
+		, "maximumDrawDowns":
+		[
+			{"month":"2014-02","Fund":0,"Benchmark":1},
+			{"month":"2014-03","Fund":0,"Benchmark":0},
+			{"month":"2014-04","Fund":0,"Benchmark":0},
+			{"month":"2014-05","Fund":0,"Benchmark":0},
+			{"month":"2014-06","Fund":0,"Benchmark":0},
+			{"month":"2014-07","Fund":0,"Benchmark":0},
+			{"month":"2014-08","Fund":0,"Benchmark":-0.59573},
+			{"month":"2014-09","Fund":0,"Benchmark":-2.741465602},
+			{"month":"2014-10","Fund":0,"Benchmark":-1.901657609},
+			{"month":"2014-11","Fund":0,"Benchmark":-0.744205077},
+			{"month":"2014-12","Fund":-0.37584,"Benchmark":-0.895872895},
+			{"month":"2015-01","Fund":0,"Benchmark":0},
+			{"month":"2015-02","Fund":0,"Benchmark":0},
+			{"month":"2015-03","Fund":-0.74559,"Benchmark":-0.53691},
+			{"month":"2015-04","Fund":0,"Benchmark":0},
+			{"month":"2015-05","Fund":-4.48951,"Benchmark":-3.40318},
+			{"month":"2015-06","Fund":-5.333230567,"Benchmark":-4.261094997},
+			{"month":"2015-07","Fund":-3.939934521,"Benchmark":-4.094226873},
+			{"month":"2015-08","Fund":-8.026934885,"Benchmark":-6.850189555},
+			{"month":"2015-09","Fund":-8.46825844,"Benchmark":-8.048682961},
+			{"month":"2015-10","Fund":-3.088187429,"Benchmark":-1.855738771},
+			{"month":"2015-11","Fund":-4.717090865,"Benchmark":-4.662360395},
+			{"month":"2015-12","Fund":-6.643577892,"Benchmark":-6.92916055}
+		]
+
+		,"riskStats":
+		[
+            { "riskElementName": "Monthly standard deviation annualised (%)", "riskValueFund": "9.23", "riskValueBenchmark": "9.21" },
+            { "riskElementName": "Positive months (%)", "riskValueFund": "62.50", "riskValueBenchmark": "54.17" },
+            { "riskElementName": "Maximum drawdown (%)", "riskValueFund": "-9.61", "riskValueBenchmark": "-10.12" },
+            { "riskElementName": "Outperformance annualised (%)", "riskValueFund": "2.52", "riskValueBenchmark": "" },
+            { "riskElementName": "Months outperformed benchmark (%)", "riskValueFund": "58.33", "riskValueBenchmark": "" },
+            { "riskElementName": "Highest Annual Performance (%) *", "riskValueFund": "21.83", "riskValueBenchmark": "16.04" },
+            { "riskElementName": "Lowest Annual Performance (%) **", "riskValueFund": "-2.44", "riskValueBenchmark": "-3.80" }
+        ]
+	}
+
+	,"exposure":
+	{
+		"assetClasses":
+		[
+		 	{ "exposure": "South African Derivatives", "val": 1.06},
+		    { "exposure": "South African Property", "val": 4.34 }, 
+		    { "exposure": "South African Cash", "val": 10.92}, 
+		    { "exposure": "South African Equities", "val": 50.68}
+		]
+
+		,"sectors":
+		[
+			{"exposure":"Bonds","val":32.6294},
+			{"exposure":"Cash","val":16.5084},
+			{"exposure":"Personal & HouseholdGoods","val":5.7611},
+			{"exposure":"Real Estate","val":5.3815},
+			{"exposure":"Health Care","val":5.1765},
+			{"exposure":"Food & Beverage","val":4.2407},
+			{"exposure":"Banks","val":4.1049},
+			{"exposure":"Technology","val":3.5618},
+			{"exposure":"Industrial Goods & Services","val":2.978},
+			{"exposure":"Media","val":2.9076},
+			{"exposure":"Retail","val":2.8474},
+			{"exposure":"Insurance","val":2.7669},
+			{"exposure":"Oil & Gas","val":2.4121},
+			{"exposure":"Travel & Leisure","val":2.2649},
+			{"exposure":"Telecommunications","val":1.6124},
+			{"exposure":"Basic Resources","val":1.5195},
+			{"exposure":"Other","val":3.325}
+		]
+
+		,"creditRatings":
+		[
+			{"exposure":"AAA", "val":65.34},
+			{"exposure":"AAA+", "val":25.34},
+			{"exposure":"AAAB", "val":5.34},
+			{"exposure":"AABB", "val":2.34},
+			{"exposure":"BBaa", "val":1.34}		
+		]
+
+		,"managers":
+		[
+			{"exposure":"Allan Gray", "val":65.34},
+			{"exposure":"Prescient", "val":25.34},
+			{"exposure":"STANLIB", "val":5.34},
+			{"exposure":"ABSA", "val":2.34},
+			{"exposure":"Coronation", "val":1.34}
+		]
+		,"underlyingFunds":
+		[
+			{"exposure":"Allan Gray Equity C", "val":65.34},
+			{"exposure":"Prescient Global Emerging Markets Fund", "val":25.34},
+			{"exposure":"STANLIB Corporate Money Market Fund", "val":5.34},
+			{"exposure":"ABSA Prime Yield Plus", "val":2.34},
+			{"exposure":"Coronation Equity", "val":1.34}
+		]
+
+		,"topHoldings":
+		[
+			{"exposure":"Cash", "val":5.56},
+			{"exposure":"Derivatives", "val":3.54},
+			{"exposure":"Naspers Ltd", "val":2.89},
+			{"exposure":"Prescient Flexible Global Income USD Fund ZAR Class D", "val":2.81},
+			{"exposure":"SAB Miller Plc", "val":2.60},
+			{"exposure":"British American Tobacco Plc", "val":2.30},
+			{"exposure":"Woolworths Holdings Ltd", "val":2.09},
+			{"exposure":"Compagnie Fin Richemont", "val":1.95},
+			{"exposure":"Investec NCD 110716 7.42%", "val":1.90},
+			{"exposure":"Other", "val":72.67}
+		]
+	}
+
+
+	,"income":
+	[
+	    { "date": "Jan 2015", "interest": 6.7, "dividends": 28.6, "REITS": 5.1 },
+	    { "date": "Feb 2015", "interest": 3, "dividends": 0, "REITS": 0},
+	    { "date": "Mar 2015", "interest": 0, "dividends": 0, "REITS": 0 },
+	    { "date": "Apr 2015", "interest": 9.6, "dividends": 43.4, "REITS": 9},
+	    { "date": "May 2015", "interest": 0, "dividends": 0, "REITS": 0 },
+	    { "date": "Jun 2015", "interest": 0, "dividends": 0, "REITS": 0},
+	    { "date": "Jul 2015", "interest": 13.5, "dividends": 49, "REITS": 5.8 },
+	    { "date": "Aug 2015", "interest": 0, "dividends": 0, "REITS": 0},
+	    { "date": "Sep 2015", "interest": 0, "dividends": 0, "REITS": 0},
+	    { "date": "Oct 2015", "interest": 11.6, "dividends": 33.4, "REITS": 1},
+	    { "date": "Nov 2015", "interest": 0, "dividends": 0, "REITS": 0 },
+	    { "date": "Dec 2015", "interest": 0, "dividends": 0, "REITS": 0}
+	]
+
+
+	,"fundManagers":
+	[
+		{
+			"name":"David Green, CFA"
+			,"title":"Chief Investment Officer"
+			,"cvShort":"Since the late 1990's, David has held senior investment management roles at ACSIS, NedGroup Investments and PPS Investments. Before joining the financial services sector, he spent almost a decade as a commercial and litigation attorney."
+			,"profilePic":"assets/images/DG.png"
+		},
+		{
+			"name":"Conlias Mancuveni, FRM"
+			,"title":"Head: Investment Research"
+			,"cvShort":"Conlias joined Hollard Investments as a co-fund manager in 2013. Prior to this, he spent 5 years as an investment analyst with PPS MultiManagers and 1 year as a quantitative analyst with Imalivest Asset Management. Conlias heads the asset allocation research process and is a co-fund manager across the multiasset class funds of funds and the building block funds range."
+			,"profilePic":"assets/images/CM.jpg"
+		}
+	]
+
+
+}
